433dbefff9d0df7499b7d01516daf34398bca4ff
subbarajpenmetsa <subbarajpenmetsa@gmail.com>
2021-09-19 11:38:28 +0530

    Update palindromecheck.py

--- palindromecheck.py
+++ palindromecheck.py
@@ -1,9 +1,10 @@
-#Check the given string is palindrom or not
+# Task-3: Check the given string is palindrom or not
+
 import sys
 s = str(sys.argv[1])
 #String Reverse
 reverse=s[-1::-1]
 if (s == reverse):
-    print("Provided string \"" + s + "\" is palindrome")
+    print("Provided string \"" + s + "\" is Palindrome")
 else:
-    print("Provided string \"" + s + "\" is not palindrome")
+    print("Provided string \"" + s + "\" is not Palindrome")
